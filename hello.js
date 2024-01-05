7a6f0a897470e6562c272563aeace5400f10ef6e
Anitasilvi <anitaferdina06@gmail.com>
2024-01-05 18:28:54 +0700

    upgrade & update library

--- hello.js
+++ hello.js
@@ -1,6 +1,9 @@
 import { sayHello, sum } from "anita-nodejs-npm-library";
-
+import {max, min} from "anita-nodejs-npm-library/number.js";
 console.info(sayHello("Anita"));
 
 const numbers = [10, 10, 10, 10, 10];
 console.info(sum(numbers));
+
+console.info(min(10, 20));
+console.info(max(10, 20));
